0c9d94d98942d4bf7e12f06477fed5a9951b02b2
Felipe Maines <felipemaines123@gmail.com>
2023-11-26 20:04:21 -0300

    mudando apiurl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:32770/api/'
+    apiUrl: 'https://localhost:7181/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:32770/api/'
+    apiUrl: 'https://localhost:7181/api/'
 };
 
