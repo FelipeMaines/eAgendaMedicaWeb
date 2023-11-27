d3f2cf936e6dd8413d18b6a2499e5cad73754e69
Felipe Maines <felipemaines123@gmail.com>
2023-11-27 16:19:30 -0300

    arrumando icons

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { LocalStorageService } from 'src/app/auth/services/local-storage.service';
+import { UsuarioService } from '../services/usuario.service';
+import { Router } from '@angular/router';
+import { NotificationService } from '../services/notification.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +14,25 @@ export class NavbarComponent {
   events: string[] = [];
   opened: boolean = true;
 
-  // links = [{
-  //  link: 'medico'
-  //  nome: 
-  // }]
+  constructor(private authService: AuthService, private localStorageService: LocalStorageService,private usuarioService: UsuarioService, private router: Router, private notificacao: NotificationService){
+
+  }
+
+  sair(){
+    console.log('sair');
+    this.authService.logout().subscribe({
+      next: () => this.processarSucesso(),
+      error: (err) => this.processarErro(err)
+    });
+  }
+
+  private processarSucesso() {
+    this.localStorageService.limparDadosLocais();
+    this.usuarioService.logout();
+    this.router.navigate(['/conta/autenticar']);
+  }
+
+  private processarErro(erro: any) {
+    this.notificacao.erro(erro);
+  }
 }
