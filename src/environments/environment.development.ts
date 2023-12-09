766a0ef51b81e0216c7005390cc28dccfcaa5780
Felipe Maines <felipemaines123@gmail.com>
2023-12-09 15:15:40 -0300

    apiUlr

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-     //apiUrl: 'https://e-agenda-medica-api-bal6.onrender.com/api/'
-     apiUrl: 'https://localhost:7181/api/'
+     apiUrl: 'https://e-agenda-medica-api-bal6.onrender.com/api/'
+     //apiUrl: 'https://localhost:7181/api/'
 };
